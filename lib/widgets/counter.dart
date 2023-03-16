e737dcec58c2d1c7736351f59ff28e0aae01b71b
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-03-16 23:56:01 +0000

    counter done

--- lib/widgets/counter.dart
+++ lib/widgets/counter.dart
@@ -1,3 +1,4 @@
+import 'package:covid_19/constants.dart';
 import 'package:flutter/material.dart';
 
 class Counter extends StatelessWidget {
@@ -14,6 +15,37 @@ class Counter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(6),
+          height: 25,
+          width: 25,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color.withOpacity(.26),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.transparent,
+              border: Border.all(
+                color: color,
+                width: 2,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          '$number',
+          style: TextStyle(
+            fontSize: 40,
+            color: color,
+          ),
+        ),
+        Text(title, style: kSubTextStyle),
+      ],
+    );
   }
 }
