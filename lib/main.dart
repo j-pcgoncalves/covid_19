4b53e3bdc10aaed246c835bb670959acddd7ad61
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-03-17 00:02:00 +0000

    home screen done

--- lib/main.dart
+++ lib/main.dart
@@ -177,6 +177,44 @@ class _HomeScreenState extends State<HomeScreen> {
                       ],
                     ),
                   ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const <Widget>[
+                      Text(
+                        'Spread of Virus',
+                        style: kTitleTextStyle,
+                      ),
+                      Text(
+                        'See details',
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    padding: const EdgeInsets.all(20),
+                    height: 178,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: const Offset(0, 10),
+                          blurRadius: 30,
+                          color: kShadowColor,
+                        ),
+                      ],
+                    ),
+                    child: Image.asset(
+                      'assets/images/map.png',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
                 ],
               ),
             ),
